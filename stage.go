c1e8d43ec04c16f1350cb8d258ab684c106f7669
monochromegane <dev.kuro.obi@gmail.com>
2020-05-27 10:36:00 +0900

    Stage will output result.

--- stage.go
+++ stage.go
@@ -1,21 +1,39 @@
 package stage
 
+import "os"
+
 type Stage struct {
+	outDir string
 }
 
-func New() *Stage {
-	return &Stage{}
+func New(outDir string) *Stage {
+	return &Stage{
+		outDir: outDir,
+	}
 }
 
-func (s *Stage) Run(iter int, newActorFn NewActorFn, scenario Scenario) {
+func (s *Stage) Run(iter int, newActorFn NewActorFn, scenario Scenario) error {
+	err := ensureOutDir(outDir)
+	if err != nil {
+		return err
+	}
+
 	for i := 0; i < iter; i++ {
 		actor := NewActorFn()
 		s.run(i, actor, scenario)
 	}
+	return nil
 }
 
 func (s *Stage) run(iter int, actor Actor, scenario Scenario) {
 	for scenario.Scan() {
 		actor.Act(scenario.Line())
 	}
+}
+
+func ensureOutDir(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, 0755)
+	}
+	return nil
 }
